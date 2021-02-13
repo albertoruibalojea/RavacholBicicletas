48feae1b14ef356c827cfebfec2a825c002dda08
albertoruibalojea <bertlerez@outlook.es>
2021-02-13 18:13:54 +0100

    Add files via upload
    proyecto subido

---
+++ Ravachol_v2/scripts/catalogo.js
@@ -0,0 +1,89 @@
+//Get product window
+function Prompt() {
+    //Ingresamos un mensaje a mostrar
+    var dni = prompt("Introduce tu DNI para reservar", "");
+    //Detectamos si el usuario ingreso un valor
+    if (dni != null) {
+        alert("Bici reservada a " + dni + ". Tienes 1 semana para recogerla, gracias.");
+    }
+    //Detectamos si el usuario NO ingreso un valor
+    else {
+        alert("No has ingresado un DNI válido");
+    }
+}
+
+
+//Filter functions
+filterSelection("all")
+filterSelectionAge("ageAll")
+
+function filterSelection(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+function filterSelectionAge(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterAge");
+    if (c == "ageAll") c = "";
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+function filterSelectionPrice(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterAge");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+function filterSelectionType(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterAge");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+function filterSelectionMore(c) {
+    var x, i;
+    x = document.getElementsByClassName("filterAge");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }
+}
+
+function w3AddClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
+    }
+}
+
+function w3RemoveClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
